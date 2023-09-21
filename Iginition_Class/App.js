cb58c00b93fba7c314e233cdc56abe982027f8a2
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-09-21 20:41:42 +0530

    add food vila layout

--- Iginition_Class/App.js
+++ Iginition_Class/App.js
@@ -1,23 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
+// const AppLayout = () => {
+//   return {
+//     /**
+//      * Header
+//      *    -Logo
+//      *    -Nav-List(Right Side)
+//      *    -Cart
+//      * Body
+//      *    -Search Bar
+//      *    -Resturant Card
+//      *            -Image
+//      *            -Name
+//      *            -Rating
+//      *            -Cusisine
+//      *    -
+//      * Footer
+//      *     -Links
+//      *     -CopyRight
+//      */
+//   };
+// };
+
+
 // JSX => React.createElement => Object => HTML(DOM)
 
 // React Element :-
 
 const head = (
   <h1 id="h1" key={"h2"}>
-    Namate React
+    Food Villa
   </h1>
 );
 const container = [head];
-
-// Components :-
-
-// 1. Functional Component
-// 2. Class Based Component
-
-// 1. Functional Component:-
 
 const HeaderComponent = () => {
   return (
@@ -32,3 +49,5 @@ const HeaderComponent = () => {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<HeaderComponent />);
+
+
